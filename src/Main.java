9363a681c5b845c0627b06b5e69224c1aca55b38
Nastya Bilida <abilida20@gmail.com>
2019-11-26 17:41:29 +0300

    fixed minor bugs

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Salad salad = new Salad("Summer salad", new RootCrops("TOMATO", 100),
                         new RootCrops("CARROT", 100), new NightshadeFamily("CUCUMBER", 50),
                         new NightshadeFamily("PEPPER", 250));
-        salad.showIngredientsByCalories (26, 500);
+        salad.showIngredientsByCalories(26, 500);
         salad.showRecipe ();
         salad.sortIngredientsByClassificationVegetable ();
     }
